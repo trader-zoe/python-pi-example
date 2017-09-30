c0fb82b1464fd800ee0732137ec4ca4f4cff61e6
hang <robert91.yin@gmail.com>
2017-09-30 17:27:19 -0600

    Newest commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-pi-example
 This is an example project to demonstrate how to use git in Ubuntu
+
+Make a change
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def main(msg):
+    //Another comment
     //Add a another comment
     //print a message
     print(msg)
