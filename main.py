6582c51235aff60523769c3c64784aebb37a5883
hang <robert91.yin@gmail.com>
2017-09-30 17:14:47 -0600

    change again

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def main(msg):
-<<<<<<< HEAD
-    \\Add a another comment
-=======
+    //Add a another comment
     //print a message
->>>>>>> refs/remotes/origin/master
     print(msg)
 
 main("Hello people")
